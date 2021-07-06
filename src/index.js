0a6518b0f77e2ea11824aeeb4b19628f3423fff2
combo <combo@combos-MacBook-Pro.local>
2021-07-06 17:44:46 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HeaderComponent from './components/Header'
+import FooterComponent from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
+    <HeaderComponent />
     <App />
+    <FooterComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
